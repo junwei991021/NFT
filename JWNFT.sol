9efd9a1ea2994ee46802217e1dc2a52156ae9e42
junwei991021 <68406522+junwei991021@users.noreply.github.com>
2023-04-19 14:54:23 +0800

    Add files via upload

---
+++ JWNFT.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+// by 0xAA
+pragma solidity ^0.8.4;
+
+import "./ERC721.sol";
+
+library MerkleProof {
+    /**
+     * @dev 当通过`proof`和`leaf`重建出的`root`与给定的`root`相等时，返回`true`，数据有效。
+     * 在重建时，叶子节点对和元素对都是排序过的。
+     */
+    function verify(
+        bytes32[] memory proof,
+        bytes32 root,
+        bytes32 leaf
+    ) internal pure returns (bool) {
+        return processProof(proof, leaf) == root;
+    }
+
+    /**
+     * @dev Returns 通过Merkle树用`leaf`和`proof`计算出`root`. 当重建出的`root`和给定的`root`相同时，`proof`才是有效的。
+     * 在重建时，叶子节点对和元素对都是排序过的。
+     */
+    function processProof(bytes32[] memory proof, bytes32 leaf) internal pure returns (bytes32) {
+        bytes32 computedHash = leaf;
+        for (uint256 i = 0; i < proof.length; i++) {
+            computedHash = _hashPair(computedHash, proof[i]);
+        }
+        return computedHash;
+    }
+    
+    // Sorted Pair Hash
+    function _hashPair(bytes32 a, bytes32 b) private pure returns (bytes32) {
+        return a < b ? keccak256(abi.encodePacked(a, b)) : keccak256(abi.encodePacked(b, a));
+    }
+}
+
+contract JWNFT is ERC721{
+    bytes32 immutable public root; // Merkle书的根
+    mapping(address => bool) public mintedAddress;   // 记录已经mint的地址
+    uint public MAX_APES = 10000; // 总量
+
+    // 构造函数
+    constructor(string memory name_, string memory symbol_, bytes32 merkleroot) ERC721(name_, symbol_){
+        root = merkleroot;
+    }
+
+    //BAYC的baseURI为ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/ 
+    function _baseURI() internal pure override returns (string memory) {
+        return "ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/";
+    }
+    
+    // 铸造函数
+    function mint(uint tokenId, bytes32[] calldata proof) external {
+        address to = msg.sender;
+        require(tokenId >= 0 && tokenId < MAX_APES, "tokenId out of range");
+        require(_verify(_leaf(to), proof), "Invalid merkle proof"); // Merkle检验通过
+       
+        
+        
+        _mint(to,tokenId);
+    }
+
+    // 计算Merkle书叶子的哈希值
+    function _leaf(address account)
+    internal pure returns (bytes32)
+    {
+        return keccak256(abi.encodePacked(account));
+    }
+
+    // Merkle树验证，调用MerkleProof库的verify()函数
+    function _verify(bytes32 leaf, bytes32[] memory proof)
+    internal view returns (bool)
+    {
+        return MerkleProof.verify(proof, root, leaf);
+    }
+}
+
